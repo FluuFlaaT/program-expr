37a23ff1fbd8d99987866daf6415f2fe9267ede6
FluuFlaaT <byd2004@gmail.com>
2024-03-23 16:37:36 +0800

    finished

--- src/core/service.c
+++ src/core/service.c
@@ -32,7 +32,33 @@ void addCard()
 }
 
 void queryCard(){
-
+    //char Input[100];
+    document * New = malloc(sizeof(document));
+    printf("请输入该卡的卡号：");
+    scanf("%s", New->cardNumber);
+    //printf("\n");
+    if(strlen(New->cardNumber) > 18)
+    {
+        printf("卡片长度非法！\n");
+    }
+    else if(!checkIfExist(Card, New))
+    {
+        printf("卡号不存在！\n");
+    }
+    else
+    {
+        document * tmp = Card;
+        for(int i = 0; i < Card->cardNum; i++)
+        {
+            tmp = tmp->next;
+            if(!strcmp(tmp->cardNumber, New->cardNumber))
+            {
+                printf("%-10s\t%-10s\t%-10s\t%-10s\t%-10s\t%-10s\n", "卡号","状态","余额","累计使用","使用次数","上次使用时间");
+                printf("%-10s\t%-10d\t%-10d\t%-10d\t%-10d\t%d-%d-%d %02d:%02d:%02d\n", tmp->cardNumber, tmp->Flag_Illegal, tmp->balance, tmp->summary, tmp->usedTime, tmp->date.Year, tmp->date.Month, tmp->date.Day, tmp->date.Hour, tmp->date.Minute, tmp->date.Second);
+                break;
+            }
+        }
+    }
 }
 
 void online(){
